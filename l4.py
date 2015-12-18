9b93fcaeee2e448c634312e4079a0180fd197e3f
ClaraBing <liubb16@connect.hku.hk>
2015-12-18 23:13:34 +0800

    Create l4.py

---
+++ l4.py
@@ -0,0 +1,18 @@
+def med(m, n):
+	l = len(m) + len(n)
+	if l % 2 == 0:
+		return (get(m, n, l//2) + get(m, n, l//2+1)) / 2
+	return get(m, n, l - l//2)
+
+def get(m, n, k):
+	if k != int(k):
+		return -1
+	k = int(k)
+	if m == []: return n[k-1]
+	if n == []: return m[k-1]
+	if k == 1:
+		return min(m[0], n[0])
+	t1, t2 = min(len(m)-1, k // 2 - 1), min(len(n)-1, k // 2 - 1)
+	if m[t1] >= n[t2]:
+		return get(m, n[t2+1:], k-t2-1)
+	return get(m[t1+1:], n, k-t1-1)
